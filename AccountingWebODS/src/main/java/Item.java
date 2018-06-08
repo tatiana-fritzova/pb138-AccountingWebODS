dd14e67e552b985f5408a58cac1f8efc561d7074
Aneta Moravčíková <xmoravc1@fi.muni.cz>
2018-06-08 12:05:42 +0200

    Added equals and toString to Item.java

--- AccountingWebODS/src/main/java/Item.java
+++ AccountingWebODS/src/main/java/Item.java
@@ -1,3 +1,6 @@
+
+import java.util.Objects;
+
 public class Item {
     private String description;
     private Double price;
@@ -13,5 +16,41 @@ public class Item {
 
     public Double getPrice() {
         return price;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+    
+    public void setPrice(double price){
+        this.price = price;
+    }
+    
+    @Override
+    public String toString(){
+        return "Item{" +
+                "description=" + description +
+                ", price=" + price + "}";
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.description, this.price);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        
+        final Item other = (Item) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.price)) {
+            return false;
+        }
+        return true;
     }
 }
